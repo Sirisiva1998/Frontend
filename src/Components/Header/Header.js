95f91cc001817108358d299d57f2acbf96837763
Sirisiva1998 <Sirisiva1998@gmail.com>
2024-03-12 14:59:33 +0530

    Responsive

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,7 +6,7 @@ import cart from '../../Assets/cart.png';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import logoutButton from '../../Api/Logout';
-
+import menu from '../../Assets/menu.png';
 function Header()
 {
     const navigate=useNavigate();
@@ -42,16 +42,22 @@ function Header()
                 <button type='button' onClick={()=>loginCheck()}>{loginButton}</button>
             </div>
              <div class="NavigationHeader">
+                {/* <img src={menu} width="30" height="30" className='menuHeader'/> */}
+                <div className='NavSubHeader'>
                 <img src={homeIcon} width='30' height='30'onClick={()=>window.location.href="/"}/>
                 <div className='hoverButton' onClick={()=>window.location.href="/BestSeller"}><h5>BestSellers</h5></div>
                 <div className='hoverButton' onClick={()=>window.location.href="/NewArrivals"}><h5 >New Arrivals</h5></div>
                 <div className='hoverButton'><h5>Pre Orders</h5></div>
                 <div className='hoverButton'><h5>Children & Young Adult</h5></div>
                 <div className='hoverButton'><h5>Fiction & Non Fiction</h5></div>
+                </div>
+                <div className='NavSubHeaderTwo'>
                 <div className='hoverButton'><h5>School Education</h5></div>
                 <div className='hoverButton'><h5>Higher Education</h5></div>
                 <div className='hoverButton'><h5>Games & Puzzles</h5></div>
                 <img src={cart} width='30' height='30' onClick={()=>window.location.href="/Cart"}/>
+                </div>
+              
             </div>
         </div>
     );
